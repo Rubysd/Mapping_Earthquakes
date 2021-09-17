51fff1812eec156c0a342028d4c600f0c7147e12
Cielo GzVerduzco <cgzverduzco@users.noreply.github.com>
2021-09-16 20:32:15 -0500

    Add code

--- Mapping_GeoJSON_Linestrings/static/js/logic.js
+++ Mapping_GeoJSON_Linestrings/static/js/logic.js
@@ -7,9 +7,15 @@ let cityData = cities;
 //let map = L.map('mymap').setView([36.1733, -120.1794],7); 
 
 // Create the map object with center and zoom level.
+<<<<<<< Updated upstream
 //let map = L.map('mymap').setView([30, 30], 2);
 
 /* // Add GeoJSON data.
+=======
+let map = L.map('mapid').setView([30, 30], 2);
+
+// Add GeoJSON data.
+>>>>>>> Stashed changes
 let sanFranAirport =
 {"type":"FeatureCollection","features":[{
     "type":"Feature",
@@ -27,10 +33,17 @@ let sanFranAirport =
         "geometry":{
             "type":"Point",
             "coordinates":[-122.375,37.61899948120117]}}
+<<<<<<< Updated upstream
 ]}; */
 
 // Grabbing our GeoJSON data.
 // L.geoJSON(sanFranAirport).addTo(map);
+=======
+]};
+
+// Grabbing our GeoJSON data.
+L.geoJSON(sanFranAirport).addTo(map);
+>>>>>>> Stashed changes
 
 /* // Grabbing our GeoJSON data.
 L.geoJson(sanFranAirport, {
@@ -43,6 +56,15 @@ L.geoJson(sanFranAirport, {
 
 }).addTo(map); */
 
+<<<<<<< Updated upstream
+=======
+L.geoJson(sanFranAirport, {
+  onEachFeature: function(feature, layer) {
+    console.log(layer);
+    layer.bindPopup("<h2>Airport code: " + feature.properties.faa + "</h2><hr></hr><h3> Airport name: " + feature.properties.name + "</h3>");
+   }
+}).addTo(map);
+>>>>>>> Stashed changes
 
 // We create the tile layer that will be the background of our map.
 //let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -50,6 +72,7 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/t
 //let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', { 
 //let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {        
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+<<<<<<< Updated upstream
     maxZoom: 18,
     accessToken: API_KEY
 });
@@ -58,10 +81,14 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
+=======
+    //maxZoom: 1,
+>>>>>>> Stashed changes
     accessToken: API_KEY
 });
 
 // Then we add our 'graymap' tile layer to the map.
+<<<<<<< Updated upstream
 //streets.addTo(map);
 
 
@@ -106,4 +133,3 @@ L.geoJson(data, {
     console.log(layer);
     layer.bindPopup("<h2>Airport code: " + feature.properties.faa + "</h2><hr></hr><h3> Airport name: " + feature.properties.name + "</h3>");
    }
-}).addTo(map); */
